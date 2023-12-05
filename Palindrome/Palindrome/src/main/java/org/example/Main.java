7e4d724089abbe503afad98bbdfe65adf15b7f6d
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-05 14:22:39 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ Palindrome/Palindrome/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main (String[]args)
+    {
+        int num, reverse = 0, rem, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        num = s.nextInt();
+        if (isPalindrome(num, reverse) == num)
+            System.out.println (num + " is a Palindrome");
+        else
+            System.out.println (num + " is not a Palindrome");
+    }
+    static int isPalindrome(int num, int rev)
+    {
+        if(num == 0)
+            return rev;
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+        return isPalindrome(num / 10, rev);
+    }
+}
+
