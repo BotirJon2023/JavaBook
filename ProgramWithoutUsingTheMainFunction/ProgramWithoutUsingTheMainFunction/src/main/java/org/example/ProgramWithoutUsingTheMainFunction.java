e1d2d3ae4e0048e82ff72aa994ae916658d728ee
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-09 23:15:41 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ProgramWithoutUsingTheMainFunction/ProgramWithoutUsingTheMainFunction/src/main/java/org/example/ProgramWithoutUsingTheMainFunction.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ProgramWithoutUsingTheMainFunction {
+
+    static {
+        System.out.println("Hello World!!");
+        System.exit(0);
+    }
+    //Does not work with JDK7
+}
