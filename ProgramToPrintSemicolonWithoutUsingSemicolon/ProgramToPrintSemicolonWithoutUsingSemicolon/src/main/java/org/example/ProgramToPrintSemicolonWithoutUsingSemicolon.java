785a5723f51d918245389ff89a89a9da2e35f93c
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-09 23:14:23 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ProgramToPrintSemicolonWithoutUsingSemicolon/ProgramToPrintSemicolonWithoutUsingSemicolon/src/main/java/org/example/ProgramToPrintSemicolonWithoutUsingSemicolon.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ProgramToPrintSemicolonWithoutUsingSemicolon {
+
+    public static void main(String[] args) {
+        int n;
+        char a;
+        n = 59;
+        a = (char) n;
+        System.out.println(a);
+    }
+}
