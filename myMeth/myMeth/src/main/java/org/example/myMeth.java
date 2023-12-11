b8269d31a9da54acbca878556a2cd440ef5ff5c5
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-11 11:26:33 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ myMeth/myMeth/src/main/java/org/example/myMeth.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class myMeth {
+
+    void myMeth() {
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (i == 5) return; // завершение цикла после достижения
+// значения 5
+            System.out.println();
+        }
+   }
+}
