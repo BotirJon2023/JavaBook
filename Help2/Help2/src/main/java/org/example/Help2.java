2b78cfaa8bcc7429a302d1579aba7f16da28d516
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-11 11:25:30 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ Help2/Help2/src/main/java/org/example/Help2.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Help2 {
+
+    public static void main(String args[])
+            throws java.io.IOException {
+        char choice, ignore;
+        do {
+            System.out.println("Справка:");
+            System.out.println(" 1. if ");
+            System.out.println(" 2. switch ");
+            System.out.println(" 3. for ");
+            System.out.println(" 4. while ");
+            System.out.println(" 5. do - while\n");
+
+            System.out.print("Выберите: ");
+            choice = (char) System.in.read();
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        } while (choice < '1' | choice > '5');
+    }
+}
+
