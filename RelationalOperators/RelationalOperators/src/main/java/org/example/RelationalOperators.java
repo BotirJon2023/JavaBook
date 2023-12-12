704803e9a3e403d84393b63b867d93731e9c7540
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-12 14:26:57 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ RelationalOperators/RelationalOperators/src/main/java/org/example/RelationalOperators.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class RelationalOperators {
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first integer:");
+        int a = s.nextInt();
+        System.out.print("Enter second integer:");
+        int b = s.nextInt();
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("b >= a : " + (b >= a));
+        System.out.println("b <= a : " + (b <= a));
+    }
+}
