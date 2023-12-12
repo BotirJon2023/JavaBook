5f1ca48e96c771be8be714d0a68e8d4f7d158b70
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-12 14:23:43 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ IllustrateUseOfPreAndPostIncrementAndDecrementOperators/IllustrateUseOfPreAndPostIncrementAndDecrementOperators/src/main/java/org/example/IllustrateUseOfPreAndPostIncrementAndDecrementOperators.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class IllustrateUseOfPreAndPostIncrementAndDecrementOperators {
+
+    public static void main(String[] args) {
+        int a, b, c, d, e;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any integer a:");
+        a = s.nextInt();
+        b = ++a;
+        System.out.println("Result after Pre Increment a:" + a);
+        System.out.println("Result after Pre Increment b:" + b);
+        c = a++;
+        System.out.println("Result after Pre Increment a:" + a);
+        System.out.println("Result after Post Increment c:" + c);
+        d = --a;
+        System.out.println("Result after Pre Increment a:" + a);
+        System.out.println("Result after Pre Decrement d:" + d);
+        e = a--;
+        System.out.println("Result after Pre Increment a:" + a);
+        System.out.println("Result after Post Decrement e:" + e);
+    }
+}
