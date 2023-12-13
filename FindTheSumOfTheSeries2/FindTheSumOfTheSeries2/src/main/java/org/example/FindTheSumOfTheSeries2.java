98cd448b9d3436d1094315aff86f92cb649f9747
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-13 09:34:53 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ FindTheSumOfTheSeries2/FindTheSumOfTheSeries2/src/main/java/org/example/FindTheSumOfTheSeries2.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FindTheSumOfTheSeries2 {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+        try{
+            System.out.println("Enter the number of terms in the series");
+            n = Integer.parseInt(br.readLine());
+        }catch (Exception e){
+            System.out.println("An error occurred");
+            return;
+        }
+        double sum = 0;
+        double i;
+        int j;
+        long fac;
+        for(i=1; i<=n;i++){
+            fac =1;
+            for(j=2; j<=i;j++){
+                fac*=j;
+            }
+            sum +=(1.0d/fac);
+        }
+        System.out.println("The sum is " + sum);
+    }
+}
