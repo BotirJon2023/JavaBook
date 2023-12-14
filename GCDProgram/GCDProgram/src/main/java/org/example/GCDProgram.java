c5b58f36931f64c421040db3b5d07b04e17fa2ef
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-14 15:39:05 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ GCDProgram/GCDProgram/src/main/java/org/example/GCDProgram.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import static java.lang.StrictMath.min;
+import java.util.Scanner;
+
+public class GCDProgram {
+
+    public static void main(String args[]) {
+
+        int a, b, hcf = 1;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter First Number:");
+
+        a = s.nextInt();
+
+        System.out.print("Enter Second Number:");
+
+        b = s.nextInt();
+        int n = min(a, b);
+
+        for (int i = 2; i < n; i++) {
+            while (a % i == 0 && b % i == 0) {
+                hcf = hcf * i;
+                a = a / i;
+                b = b / i;
+            }
+        }
+
+        System.out.println("Greatest Common Divisor:" + hcf);
+    }
+}
