b19b9e8149e47bec7060c951e25592b478d57766
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-18 14:40:21 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame/CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame/src/main/java/org/example/CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame implements ActionListener {
+
+    static JFrame frame;
+    //Driver function
+    public static void main(String args[])
+    {
+        //Create a frame
+        frame = new JFrame("Change Frame Background");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.white);
+        frame.setLayout(new FlowLayout());
+        //Create an object
+        CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame obj = new CreateAColorDialogBoxToChangeTheBackgroundColorOfFrame();
+        //Create a button
+        JButton button = new JButton("Change Color");
+        button.addActionListener(obj);
+        frame.add(button);
+        //Display the fame
+        frame.setVisible(true);
+    }
+    //Function to create color dialog box and change color
+    public void actionPerformed(ActionEvent e)
+    {
+        //Create a color dialog box
+        JColorChooser color_box= new JColorChooser();
+        Color color=color_box.showDialog(frame,"Select a Color",Color.white);
+        //Change background color of frame
+        frame.getContentPane().setBackground(color);
+    }
+}
