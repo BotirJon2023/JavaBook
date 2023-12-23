11e28bae9f2df47f026e4568f8b792f428da4e27
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-23 13:29:57 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ FindASCIIValueOfACharacter/FindASCIIValueOfACharacter/src/main/java/org/example/FindASCIIValueOfACharacter.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class FindASCIIValueOfACharacter {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
