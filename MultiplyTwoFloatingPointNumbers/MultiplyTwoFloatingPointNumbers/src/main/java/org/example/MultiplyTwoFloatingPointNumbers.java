9b0794ead7a740bb0d0b717340fe73bcc490381e
BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>
2023-12-23 13:30:18 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: BotirJon2023 <132999244+BotirJon2023@users.noreply.github.com>

---
+++ MultiplyTwoFloatingPointNumbers/MultiplyTwoFloatingPointNumbers/src/main/java/org/example/MultiplyTwoFloatingPointNumbers.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MultiplyTwoFloatingPointNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
