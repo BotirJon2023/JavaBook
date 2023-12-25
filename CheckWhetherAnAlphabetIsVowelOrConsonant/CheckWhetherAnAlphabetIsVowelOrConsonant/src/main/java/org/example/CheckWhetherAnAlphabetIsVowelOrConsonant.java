a1946662865deed2687cf5db62407c85ad5af0c2
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-25 10:55:39 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ CheckWhetherAnAlphabetIsVowelOrConsonant/CheckWhetherAnAlphabetIsVowelOrConsonant/src/main/java/org/example/CheckWhetherAnAlphabetIsVowelOrConsonant.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class CheckWhetherAnAlphabetIsVowelOrConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
+
