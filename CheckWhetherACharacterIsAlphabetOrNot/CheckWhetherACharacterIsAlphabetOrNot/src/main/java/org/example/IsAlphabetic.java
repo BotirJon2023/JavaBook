e00157a2b673d83c540f6c63ff04a5fb2ce24079
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-26 10:56:39 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ CheckWhetherACharacterIsAlphabetOrNot/CheckWhetherACharacterIsAlphabetOrNot/src/main/java/org/example/IsAlphabetic.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class IsAlphabetic {
+
+    public static void main(String[] args) {
+
+        char c = 'a';
+
+        if (Character.isAlphabetic(c)) {
+            System.out.println(c + " is an alphabet.");
+        } else {
+            System.out.println(c + " is not an alphabet.");
+        }
+    }
+}
