418dcf7ee1a241f4e57142df4737f6adc0214783
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-26 10:59:41 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ FindFactorialOfANumber/FindFactorialOfANumber/src/main/java/org/example/Factorial.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int num = 30;
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 1; i <= num; ++i) {
+
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
