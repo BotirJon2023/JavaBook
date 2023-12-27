bb120208c1017d2b031cca414a2107e13209c950
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-27 12:50:14 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ GenerateMultiplicationTable/GenerateMultiplicationTable/src/main/java/org/example/GenerateMultiplicationTable.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class GenerateMultiplicationTable {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for (int i = 1; i <= 10; ++i) {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
