905c02267e0eb237ffa068c67db47a613edacbe8
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-27 12:49:46 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ FindLCMOfTwoNumbers/FindLCMOfTwoNumbers/src/main/java/org/example/FindLCMOfTwoNumbers.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class FindLCMOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int n1 = 72, n2 = 120, lcm;
+
+        lcm = (n1 > n2) ? n1 : n2;
+
+        // Always true
+        while (true) {
+            if (lcm % n1 == 0 && lcm % n2 == 0) {
+                System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+                break;
+            }
+            ++lcm;
+        }
+    }
+}
