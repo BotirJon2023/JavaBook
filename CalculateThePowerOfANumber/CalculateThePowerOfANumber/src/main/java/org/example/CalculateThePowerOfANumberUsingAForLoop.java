cc3cb7dd7579cfde92819116fecc10a40794567b
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2023-12-28 11:46:17 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ CalculateThePowerOfANumber/CalculateThePowerOfANumber/src/main/java/org/example/CalculateThePowerOfANumberUsingAForLoop.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class CalculateThePowerOfANumberUsingAForLoop {
+
+    public static void main(String[] args) {
+
+        int base = 3, exponent = 4;
+
+        long result = 1;
+
+        for (; exponent != 0; --exponent) {
+            result *= base;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
---
+++ CalculateThePowerOfANumber/CalculateThePowerOfANumber/src/main/java/org/example/ComputePowerOfNegativeNumber.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ComputePowerOfNegativeNumber {
+
+    public static void main(String[] args) {
+
+        int base = -3, exponent = 2;
+
+        double result = Math.pow(base, exponent);
+        System.out.println("Answer = " + result);
+    }
+}
