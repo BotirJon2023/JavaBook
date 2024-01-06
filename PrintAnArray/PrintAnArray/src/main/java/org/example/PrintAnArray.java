1c9822f58e667b33d0fcfb3fe9a44ca9330b454a
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-06 14:15:24 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ PrintAnArray/PrintAnArray/src/main/java/org/example/PrintAnArray.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PrintAnArray {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+
+}
---
+++ PrintAnArray/PrintAnArray/src/main/java/org/example/PrintAnArray2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class PrintAnArray2 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(array));
+    }
+
+}
