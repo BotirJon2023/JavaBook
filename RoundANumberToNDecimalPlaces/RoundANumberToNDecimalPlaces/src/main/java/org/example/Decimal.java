b0b384e59c7071b17459099c006608438147b27a
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-06 14:15:48 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ RoundANumberToNDecimalPlaces/RoundANumberToNDecimalPlaces/src/main/java/org/example/Decimal.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+public class Decimal {
+
+    public static void main(String[] args) {
+        double num = 1.34567;
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        System.out.println(df.format(num));
+    }
+}
---
+++ RoundANumberToNDecimalPlaces/RoundANumberToNDecimalPlaces/src/main/java/org/example/RoundANumberToNDecimalPlaces.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class RoundANumberToNDecimalPlaces {
+
+    public static void main(String[] args) {
+        double num = 1.34567;
+
+        System.out.format("%.4f", num);
+    }
+
+}
