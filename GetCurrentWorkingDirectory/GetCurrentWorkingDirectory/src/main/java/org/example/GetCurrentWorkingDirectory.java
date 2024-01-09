619efef8c4c4dc4749f60f9d04b589cc80735e4a
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-09 10:19:02 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ GetCurrentWorkingDirectory/GetCurrentWorkingDirectory/src/main/java/org/example/GetCurrentWorkingDirectory.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class GetCurrentWorkingDirectory {
+
+    public static void main(String[] args) {
+
+        String path = System.getProperty("user.dir");
+
+        System.out.println("Working Directory = " + path);
+
+    }
+}
