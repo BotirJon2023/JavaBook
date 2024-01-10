39e4dd79a5606ac5dc1659141be5cfc553746a69
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-10 19:11:43 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ AppendTextToAnExistingFile/AppendTextToAnExistingFile/src/main/java/org/example/AppendFile.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AppendFile {
+
+    public static void main(String[] args) {
+
+        String path = System.getProperty("user.dir") + "\\src\\test.txt";
+        String text = "Added text";
+
+        try {
+            FileWriter fw = new FileWriter(path, true);
+            fw.write(text);
+            fw.close();
+        } catch (IOException e) {
+        }
+    }
+}
