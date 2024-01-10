29d6ee134af4d042e79719a4268191db53e907e7
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-10 19:12:05 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertAStackTraceToAString/ConvertAStackTraceToAString/src/main/java/org/example/ConvertAStackTraceToAString.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+public class ConvertAStackTraceToAString {
+
+    public static void main(String[] args) {
+
+        try {
+            int division = 0 / 0;
+        } catch (ArithmeticException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            System.out.println(exceptionAsString);
+        }
+    }
+}
