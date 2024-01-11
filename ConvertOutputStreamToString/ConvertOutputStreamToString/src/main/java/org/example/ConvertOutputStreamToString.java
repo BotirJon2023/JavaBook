427d02a338f518de1c93a96a7fcf5b18b338b043
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-11 10:35:39 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertOutputStreamToString/ConvertOutputStreamToString/src/main/java/org/example/ConvertOutputStreamToString.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.*;
+public class ConvertOutputStreamToString {
+
+    public static void main(String[] args) throws IOException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String line = "Hello there!";
+
+        stream.write(line.getBytes());
+        String finalString = new String(stream.toByteArray());
+
+        System.out.println(finalString);
+    }
+}
