bdb71903426ec640aa4a80a84705b8a235c8fa88
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-17 10:24:01 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertIntTypeVariablesToLong/ConvertIntTypeVariablesToLong/src/main/java/org/example/ConvertIntTypeVariablesToLong1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ConvertIntTypeVariablesToLong1 {
+
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 34;
+
+        long c = a;
+        long d = b;
+
+        System.out.println(c);    // 25
+        System.out.println(d);    // 34
+    }
+}
---
+++ ConvertIntTypeVariablesToLong/ConvertIntTypeVariablesToLong/src/main/java/org/example/ConvertIntTypeVariablesToLong2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ConvertIntTypeVariablesToLong2 {
+
+    public static void main(String[] args) {
+
+        int a = 251;
+
+        Long obj = Long.valueOf(a);
+
+        System.out.println(obj);    // 251
+    }
+
+}
