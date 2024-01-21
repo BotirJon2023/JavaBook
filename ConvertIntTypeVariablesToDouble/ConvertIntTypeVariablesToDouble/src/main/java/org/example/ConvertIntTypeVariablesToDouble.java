e451cdfe8a25196366042b33abf07444f7f33533
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-01-21 18:02:40 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertIntTypeVariablesToDouble/ConvertIntTypeVariablesToDouble/src/main/java/org/example/ConvertIntTypeVariablesToDouble.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ConvertIntTypeVariablesToDouble {
+
+    public static void main(String[] args) {
+
+        int a =33;
+        int b = 29;
+
+        double c = a;
+        double d = b;
+
+        System.out.println(c);    // 33.0
+        System.out.println(d);    // 29.0
+    }
+}
---
+++ ConvertIntTypeVariablesToDouble/ConvertIntTypeVariablesToDouble/src/main/java/org/example/ConvertIntTypeVariablesToDouble2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertIntTypeVariablesToDouble2 {
+
+    public static void main(String[] args) {
+
+        int a = 332;
+
+        Double obj = Double.valueOf(a);
+
+        System.out.println(obj);    // 332.0
+    }
+}
