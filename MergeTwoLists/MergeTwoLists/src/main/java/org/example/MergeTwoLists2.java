2a3fdcc76550e2e47178e3b992dc2a4990b25d82
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-02-02 10:40:59 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ MergeTwoLists/MergeTwoLists/src/main/java/org/example/MergeTwoLists2.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MergeTwoLists2 {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> prime = new ArrayList<>(Arrays.asList(2, 3, 5));
+        System.out.println("First List: " + prime);
+
+        List<Integer> even = new ArrayList<>(Arrays.asList(4, 6));
+        System.out.println("Second List: " + even);
+
+        Stream<Integer> list1 = prime.stream();
+        Stream<Integer> list2 = even.stream();
+
+        Stream<Integer> merged = Stream.concat(list1, list2);
+
+        List<Integer> numbers = merged.collect(Collectors.toList());
+
+        System.out.println("Merged List: " + numbers);
+    }
+}
