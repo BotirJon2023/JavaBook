f8cbde3dc34168d63283882f905ceae4daa45ab9
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-02-04 16:18:29 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ SortMapByKeys/SortMapByKeys/src/main/java/org/example/SortMapByKeys.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortMapByKeys {
+
+    public static void main(String[] args) {
+
+
+        Map<String, String> languages = new HashMap<>();
+        languages.put("pos3", "JS");
+        languages.put("pos1", "Java");
+        languages.put("pos2", "Python");
+        System.out.println("Map: " + languages);
+
+
+        TreeMap<String, String> sortedNumbers = new TreeMap<>(languages);
+        System.out.println("Map with sorted Key" + sortedNumbers);
+
+    }
+}
