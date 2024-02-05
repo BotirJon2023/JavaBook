4810986a4a52a6aea3ac6326380596c5fb9e1d15
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-02-05 11:53:06 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ PerformThePreorderTreeTraversal/PerformThePreorderTreeTraversal/src/main/java/org/example/PerformThePreorderTreeTraversal.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class PerformThePreorderTreeTraversal {
+
+    public static void main(String[] args) {
+
+        Tree tree = new Tree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(12);
+        tree.root.right = new Node(9);
+        tree.root.left.left = new Node(5);
+        tree.root.left.right = new Node(6);
+
+        System.out.println("\nPreorder traversal ");
+        tree.preorder(tree.root);
+    }
+}
