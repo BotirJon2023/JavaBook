e12ab65b64580dca656379994198b2acbbe2477a
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-02-11 05:06:51 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ TextBasedNoteTakingApplication/TextBasedNoteTakingApplication/src/main/java/org/example/Note.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Note {
+    public Note(String title, String content, String category) {
+    }
+
+    public void setContent(String text) {
+    }
+
+    public String getTitle() {
+        return null;
+    }
+}
