f15c54512617b0e4708027597f1882015432465f
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-02-22 00:55:38 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ OnlineReservationSystemUsingJava/OnlineReservationSystemUsingJava/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ReservationSystemUI obj = new ReservationSystemUI();
+        obj.start();
+    }
+}
---
+++ OnlineReservationSystemUsingJava/OnlineReservationSystemUsingJava/src/main/java/org/example/OnlineReservationSystemUsingJava.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class OnlineReservationSystemUsingJava {
+
+
+
+}
