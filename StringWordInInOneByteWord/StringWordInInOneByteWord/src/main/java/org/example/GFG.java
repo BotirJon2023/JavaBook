9cd43a2e083ef25ce42fcd64cf6c0f9e3e5598d9
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-03-16 07:19:31 +0100

    Add files via upload
    Learning Java Programming Language.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ StringWordInInOneByteWord/StringWordInInOneByteWord/src/main/java/org/example/GFG.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class GFG {
+
+
+    public static byte convertStringToByte(String str) {
+
+
+        return Byte.parseByte(str);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        String stringValue = "1";
+
+
+        byte byteValue;
+
+
+        byteValue = convertStringToByte(stringValue);
+
+
+        System.out.println(
+                stringValue
+                        + " after converting into byte = "
+                        + byteValue);
+    }
+}
