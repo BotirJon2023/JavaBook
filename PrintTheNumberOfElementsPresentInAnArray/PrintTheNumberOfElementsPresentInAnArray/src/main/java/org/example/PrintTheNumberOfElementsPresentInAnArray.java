4f74892ad3e48d8e7e7d8251dab50e4c7fdb52e9
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-05-06 04:26:32 +0200

    Add files via upload
    Java code examples related to Array.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ PrintTheNumberOfElementsPresentInAnArray/PrintTheNumberOfElementsPresentInAnArray/src/main/java/org/example/PrintTheNumberOfElementsPresentInAnArray.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PrintTheNumberOfElementsPresentInAnArray {
+
+    public static void main(String[] args) {
+
+        int [] arr = new int [] {1, 2, 3, 4, 5};
+
+        System.out.println("Number of elements present in given array: " + arr.length);
+    }
+
+}
