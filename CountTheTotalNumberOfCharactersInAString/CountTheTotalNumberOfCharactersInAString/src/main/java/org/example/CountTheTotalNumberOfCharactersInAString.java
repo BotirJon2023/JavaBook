b40c83c26824789c9f0b0963d7a63dfc89113c8b
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-06-11 05:08:31 +0200

    Add files via upload
    Count the characters in String with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ CountTheTotalNumberOfCharactersInAString/CountTheTotalNumberOfCharactersInAString/src/main/java/org/example/CountTheTotalNumberOfCharactersInAString.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class CountTheTotalNumberOfCharactersInAString {
+
+    public static void main(String[] args) {
+
+        String string = "The best of the best in the both worlds championships";
+
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ')
+                count++;
+        }
+
+        System.out.println("Total number of characters in a string: " + count);
+    }
+
+}
