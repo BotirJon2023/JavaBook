29b1bdf77c8105ebc74304e743c10c8988a102a2
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-06-29 14:44:33 +0200

    Add files via upload
    Managing how find the largest and the smallest word in a string with Java code example.
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

--- FindTheLargestAndSmallestWordInAString/FindTheLargestAndSmallestWordInAString/src/main/java/org/example/FindTheLargestAndSmallestWordInAString.java
+++ FindTheLargestAndSmallestWordInAString/FindTheLargestAndSmallestWordInAString/src/main/java/org/example/FindTheLargestAndSmallestWordInAString.java
@@ -1,43 +1,50 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class FindTheLargestAndSmallestWordInAString {
 
-    static void printLargestAndSmallestWord(String str) {
-        String[] arr = str.split(" ");
-        int i = 0;
-        int maxlength, minlength;
-        maxlength = Integer.MIN_VALUE;
-        minlength = Integer.MAX_VALUE;
-        String largest, smallest;
-        largest = smallest = "";
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i].length() < minlength) {
-                smallest = arr[i];
-                minlength = arr[i].length();
-            }
-            if (arr[i].length() > maxlength) {
-                largest = arr[i];
-                maxlength = arr[i].length();
-            }
-        }
-        System.out.println("The largest and smallest word is \"" + largest +
-                "\" and \"" + smallest + "\"");
-    }
-
-    // Main function to read the string
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the text string");
-        String str;
-        try {
-            str = br.readLine();
-        } catch (Exception e) {
-            System.out.println("Error reading input");
-            return;
+
+        String string = "Hardships often prepare ordinary people for an extraordinary destiny";
+
+        String word = "", small = "", large = "";
+
+        String[] words = new String[100];
+
+        int length = 0;
+
+        string = string + " ";
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (string.charAt(i) != ' ') {
+
+                word = word + string.charAt(i);
+
+            } else {
+
+                words[length] = word;
+
+                length++;
+
+                word = "";
+            }
         }
-        printLargestAndSmallestWord(str);
+
+        small = large = words[0];
+
+        for (int k = 0; k < length; k++) {
+
+            if (small.length() > words[k].length())
+
+                small = words[k];
+
+            if (large.length() < words[k].length())
+
+                large = words[k];
+        }
+
+        System.out.println("Smallest word: " + small);
+
+        System.out.println("Largest word: " + large);
     }
 }
