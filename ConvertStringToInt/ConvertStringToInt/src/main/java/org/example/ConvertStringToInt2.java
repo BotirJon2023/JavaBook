37861049b3352365e400814a73c19f0331c68354
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-12 03:34:06 +0200

    Add files via upload
    Converting string to integer with Java code examples
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertStringToInt/ConvertStringToInt/src/main/java/org/example/ConvertStringToInt2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ConvertStringToInt2 {
+
+    public static void main(String args[]) {
+
+        String s = "200";
+
+        Integer i = Integer.valueOf(s);
+
+        System.out.println(i);
+    }
+
+}
---
+++ ConvertStringToInt/ConvertStringToInt/src/main/java/org/example/ConvertStringToInt3.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertStringToInt3 {
+
+    public static void main(String args[]){
+
+        String s="hello";
+
+        int i=Integer.parseInt(s);
+
+        System.out.println(i);
+    }
+}
