56e6c3bbb0b36d2898dd5713cebbc576836780de
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-15 11:42:21 +0200

    Add files via upload
    Converting long to string with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ LongToString/LongToString/src/main/java/org/example/LongToString.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class LongToString {
+
+    public static void main(String args[]) {
+
+        long i = 9993939399L;
+
+        String s = String.valueOf(i);
+
+        System.out.println(s);
+    }
+}
---
+++ LongToString/LongToString/src/main/java/org/example/LongToString2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class LongToString2 {
+
+    public static void main(String args[]) {
+
+        long i = 9993939399L;
+
+        String s = Long.toString(i);
+
+        System.out.println(s);
+    }
+}
