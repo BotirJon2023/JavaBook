8af1f11066044befb23291e062e45d7d2c8cc2d1
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-07-18 10:41:53 +0200

    Add files via upload
    Converting string to double with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertStringToDouble/ConvertStringToDouble/src/main/java/org/example/ConvertStringToDouble.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertStringToDouble {
+
+    public static void main(String args[]) {
+    
+        String s = "23.6";
+
+        double d = Double.parseDouble("23.6");
+
+        System.out.println(d);
+    }
+}
