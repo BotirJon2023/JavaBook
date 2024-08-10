446991008da730709fb1b8610bb37abaf2799a4f
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-10 10:45:21 +0200

    Add files via upload
    Converting decimal to hexadecimal with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ ConvertDecimalToHexadecimal/ConvertDecimalToHexadecimal/src/main/java/org/example/ConvertDecimalToHexadecimal.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConvertDecimalToHexadecimal {
+
+    public static void main(String args[]) {
+
+        System.out.println(Integer.toHexString(10));
+
+        System.out.println(Integer.toHexString(15));
+
+        System.out.println(Integer.toHexString(289));
+    }
+}
