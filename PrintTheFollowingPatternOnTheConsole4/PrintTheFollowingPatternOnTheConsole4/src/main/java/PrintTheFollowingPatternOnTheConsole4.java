0d9fddb0fd31349b773dd8d001fe925790f27b9c
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-23 13:10:51 +0200

    Add files via upload
    Printing pattern on the Console with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ PrintTheFollowingPatternOnTheConsole4/PrintTheFollowingPatternOnTheConsole4/src/main/java/PrintTheFollowingPatternOnTheConsole4.java
@@ -0,0 +1,19 @@
+public class PrintTheFollowingPatternOnTheConsole4 {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(" " + (char) (65 + i));
+
+            }
+
+            System.out.println("");
+
+        }
+    }
+}
