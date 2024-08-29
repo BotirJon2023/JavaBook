4899e869f4f19835cd9386bce2d6f88e3ab4808e
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-08-29 10:25:41 +0200

    Add files via upload
    Printing pattern with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ PrintTheFollowingPatternOnTheConsole10/PrintTheFollowingPatternOnTheConsole10/src/main/java/org/example/PrintTheFollowingPatternOnTheConsole10.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class PrintTheFollowingPatternOnTheConsole10 {
+
+    public static void main(String[] args) {
+
+        int i, j;
+
+        int n = 6;
+
+        for (i = n; i > 0; i--) {
+
+            for (j = 0; j < i; j++) {
+
+                System.out.print("5");
+
+            }
+
+            System.out.println(" ");
+
+        }
+    }
+}
