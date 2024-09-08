7f361af025001948d22bf27b13261ebba5c206af
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-09-08 12:03:48 +0200

    Add files via upload
    String value to byte value with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ StringWertInJavaInEinenByteWert/StringWertInJavaInEinenByteWert/src/main/java/org/example/StringWertInJavaInEinenByteWert.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class StringWertInJavaInEinenByteWert {
+
+
+    public static byte convertStringToByte(String str) {
+
+        return Byte.parseByte(str);
+    }
+
+
+    public static void main(String[] args) {
+
+        String stringValue = "1";
+
+        byte byteValue;
+
+        byteValue = convertStringToByte(stringValue);
+
+
+        System.out.println(
+
+                stringValue
+
+                        + " after converting into byte = "
+
+                        + byteValue);
+    }
+}
