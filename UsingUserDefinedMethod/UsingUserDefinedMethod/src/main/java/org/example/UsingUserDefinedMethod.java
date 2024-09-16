409c33415d5ccbf687457264120a59f8498cd2e4
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2024-09-16 21:42:36 +0200

    Add files via upload
    Triangle area with Java code example
    
    Signed-off-by: Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>

---
+++ UsingUserDefinedMethod/UsingUserDefinedMethod/src/main/java/org/example/UsingUserDefinedMethod.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+
+public class UsingUserDefinedMethod {
+
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the width of the Triangle:");
+
+        double b = s.nextDouble();
+
+        System.out.println("Enter the height of the Triangle:");
+
+        double h = s.nextDouble();
+
+        double area = AOT(b, h);
+
+        System.out.println("Area of Triangle is: " + area);
+    }
+
+    static double AOT(double b, double h) {
+
+        return ((b * h) / 2);
+
+
+    }
+
+}
